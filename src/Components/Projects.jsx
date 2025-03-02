2984d07f24e1aab6d7e280206622e34b0acd5c2c
Ng-Kai-Huat-Jason <S10262552@connect.np.edu.sg>
2025-03-02 23:10:26 +0800

    Added Projects and on Hover

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,6 +4,7 @@ import GenshinImpactLogo from "../images/GenshinImpact.png"; // Import the image
 import Error404 from "../images/Error404.png"; // Import the placeholder image
 import Popup from "./Popup"; // Import the Popup component
 import SkillForgeLogo from "../images/skillforgeLogo.png"; // Import the image
+import PocketPlanLogo from "../images/pocketplanLogo.png"; // Import the image
 
 const Projects = () => {
   const [showPopup, setShowPopup] = useState(false); // State to control popup visibility
@@ -16,6 +17,7 @@ const Projects = () => {
         "BannerBites is a web application for managing advertisements dynamically with real-time updates.",
       githubLink: "https://github.com/Koyonari/BannerBites_FSDIT03",
       demoLink: "https://youtu.be/pAA6HHHId9w",
+      type: "github",
     },
     {
       title: "GenshinPromo",
@@ -23,20 +25,29 @@ const Projects = () => {
       description: "Front-end web application for a Genshin Impact fan site.",
       githubLink:
         "https://github.com/Ng-Kai-Huat-Jason/FED_GenshinPromo_website",
-      demoLink: "#", // No demo available
-    },
-    {
-      title: "Dummy Project",
-      description: "Dummy project description",
-      githubLink: "https://roblox.com/",
-      demoLink: "#", // No demo available
+      demoLink:
+        "https://drive.google.com/file/d/1wCEsWRCG7IbrVyfCfRt2HEUQlJSkP1z4/view?usp=sharing",
+      type: "github",
     },
     {
       title: "SkillForge",
-      image: "SkillForgeLogo",
-      description: "killForge aims to combat the issue of underemployment in Singapore directly, by empowering users to seek out learning opportunities, certficiations and job vacancies based on their newly acquired or enhanced digital skills.",
-      githubLink: "https://www.figma.com/design/sg3rw7WfjFikKCQphJtYsr/DUX_Assignment-1_Ng-Kai-Huat-Jason?node-id=0-1&t=70Bswq0PybaS3KGk-1",
+      image: SkillForgeLogo,
+      description:
+        "SkillForge aims to combat the issue of underemployment in Singapore directly, by empowering users to seek out learning opportunities, certifications and job vacancies based on their newly acquired or enhanced digital skills.",
+      githubLink:
+        "https://www.figma.com/design/sg3rw7WfjFikKCQphJtYsr/DUX_Assignment-1_Ng-Kai-Huat-Jason?node-id=0-1&t=70Bswq0PybaS3KGk-1",
       demoLink: "#", // No demo available
+      type: "figma",
+    },
+    {
+      title: "PocketPlan",
+      image: PocketPlanLogo,
+      description:
+        "PocketPlan will be an application with comprehensive functionality across budgeting, tracking and analytics. Enhanced with gamified features, PocketPlan not only helps users manage their finances but also promotes financial literacy and discipline in a fun, engaging way.",
+      githubLink:
+        "https://www.figma.com/design/1Yy6RRCdoaGxmd8EDbGFtj/Vanguard_DUX_Asg2_Prototype?node-id=0-1&t=PwSWTCYrQKyLFnum-1",
+      demoLink: "#", // No demo available
+      type: "figma",
     },
   ];
 
@@ -52,12 +63,15 @@ const Projects = () => {
   return (
     <section id="projects" className="py-20 pt-16 bg-primary">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold text-center text-background">Projects</h2>
+        <h2 className="text-3xl font-bold text-center text-background">
+          Projects
+        </h2>
         <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {projects.map((project, index) => (
             <div
               key={index}
-              className="bg-background p-6 rounded-lg shadow-lg hover:shadow-xl cursor-pointer transition-shadow duration-300 flex flex-col"
+              className="bg-background p-6 rounded-lg shadow-lg hover:shadow-xl cursor-pointer 
+             transition-transform duration-300 transform hover:scale-105 flex flex-col"
             >
               {/* Project Image */}
               <div className="flex justify-center">
@@ -70,10 +84,10 @@ const Projects = () => {
 
               {/* Project Details */}
               <div className="mt-6 flex-1 flex flex-col">
-                <h3 className="text-xl font-bold text-primary">{project.title}</h3>
-                <p className="mt-2 text-black flex-1">
-                  {project.description}
-                </p>
+                <h3 className="text-xl font-bold text-primary">
+                  {project.title}
+                </h3>
+                <p className="mt-2 text-black flex-1">{project.description}</p>
                 <div className="mt-4">
                   <a
                     href={project.githubLink}
@@ -81,7 +95,8 @@ const Projects = () => {
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    GitHub
+                    {project.type === "github" ? "Github" : "View on Figma"}{" "}
+                    {/* Dynamic link text */}
                   </a>
                   <button
                     onClick={() => handleDemoClick(project.demoLink)}
